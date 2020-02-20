0e163464c1ef69991198c6ad77ab38afb18f67f6
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2020-02-20 13:09:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# GraphQL Sample App
+
+A full-stack application, including a GraphQL server on Node.js, a React front-end (with Apollo), MongoDB and MongoDB Atlas (ex mLab) to store all the data.
+
+### How to use
+
+1) Clone or download repo
+2) Install dependencies.
+3) Go to server folder and run
+`
+nodemon app
+` . Nodemon will restart the server for us every time we make changes
+
+4) Go to client folder and run
+`
+npm start
+`
---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require('cors');
+
+const app = express();
+
+//Allow cross-origin request
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://test:test1234@cluster0-mbiru.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to the db");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
